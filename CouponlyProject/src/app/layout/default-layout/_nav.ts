288f3cc81ab4864770bce77a8cd819b58d7150c1
anandasokandev <ianandasokan@gmail.com>
2025-07-23 14:46:11 +0530

    Location Created

--- CouponlyProject/src/app/layout/default-layout/_nav.ts
+++ CouponlyProject/src/app/layout/default-layout/_nav.ts
@@ -15,6 +15,11 @@ export const navItems: INavData[] = [
     name: 'Theme'
   },
   {
+    name: 'Location',
+    url: '/location',
+    iconComponent: { name: 'cil-drop' }
+  },
+  {
     name: 'Colors',
     url: '/theme/colors',
     iconComponent: { name: 'cil-drop' }
