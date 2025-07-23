3065bcfa8e68ef9b5820bc1e242a128a8ade96dc
ebin-vj <ebinjoseph703@gmail.com>
2025-07-23 15:38:34 +0530

    redeem history component added

---
+++ CouponlyProject/src/app/features/admin/components/redeem-history/redeem-history.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-redeem-history',
+  imports: [],
+  templateUrl: './redeem-history.component.html',
+  styleUrl: './redeem-history.component.scss'
+})
+export class RedeemHistoryComponent {
+
+}
